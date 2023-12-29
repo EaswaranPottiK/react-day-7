a197cf208780b274cd835140fba0473f92c43b43
Easwaran <easwaranpottiak@gmail.com>
2023-12-29 11:32:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,28 +4,59 @@ import Header from './Header';
 import Filter from './Filter';
 import OneCard from './OneCard';
 import Data from './Data'
+import { useState } from 'react';
 
 function App() {
 
+  const [usefulData,setUsefulData] =useState(Data)
+
   function handleSetStar(rating){
-      if(rating==='4'){
-          return('⭐⭐⭐⭐')
+      if(rating===1){
+          return('⭐')
+      }
+      else if(rating===2){
+        return('⭐⭐')
+      }
+      else if(rating===3){
+        return('⭐⭐⭐')
+      }
+      else if(rating===4){
+        return('⭐⭐⭐⭐')
       }
       else{
         return('⭐⭐⭐⭐⭐')
       }
       
   }
+
+  const [searchKey, setSearchKey] = useState('')
+  const [minRating,setMinRating] = useState(0);
+
+  function search(e){
+    if (e.target.type==='number'){
+      setMinRating(e.target.value)
+    }
+    if (e.target.type==='text'){
+      setSearchKey(e.target.value)
+    }
+
+    const filteredList = Data.filter((data)=>(
+      data.name.toLowerCase().includes(searchKey.toLowerCase()) &&
+      data.rating >= minRating
+    ))
+    setUsefulData(filteredList)
+  }
+
   return (
     <div>
       <Header />
-      <Filter/>
+      <Filter search={search}/>
       
       <div style={{display:'flex',justifyContent:'space-around',flexWrap:'wrap',paddingLeft:'1%',paddingRight:'1%'}}>
       {
-        Data.map((item,count) => (
+        usefulData.map((item,count) => (
            
-            <OneCard name={item.name} rating={handleSetStar(item.rating)} postcode={item.postcode} type={item.type} address={item.address}/>
+            <OneCard name={item.name} rating={handleSetStar(item.rating)} postcode={item.postcode} type={item.type_of_food} address={item.address}/>
           ))
       }
       </div>
--- src/Filter.js
+++ src/Filter.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Filter = () => {
+const Filter = (props) => {
   return (
     <div style={{margin:'4%',display:'flex',justifyContent:'space-between'}}>
-        <input type='text' style={{height:'35px', border:'1px solid grey', borderRadius:'4px'}} placeholder='Search restaurants...'></input>
+        <input type='text' onChange={props.search} min={0} max={5} style={{height:'35px', border:'1px solid grey', borderRadius:'4px'}} placeholder='Search restaurants...'></input>
         <div style={{display:'flex',alignItems:'center',gap:'10px'}}>
             <p>Minimum Rating:</p>
-            <input type='number' style={{height:'35px' ,width:'70px'}}></input>
+            <input onChange={props.search} type='number' placeholder='0' style={{height:'35px' ,width:'70px'}}></input>
         </div>
     </div>
   )
